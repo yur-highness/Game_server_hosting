dacd4e952a49f2bc795fc6d49b98aa9929456277
yur-highness <souvickghosh222@gmail.com>
2024-12-08 09:19:22 +0530

    update

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ let loginBtn = document.querySelector('.login-btn');
 let dropLogin = document.querySelector('.drop-login');
 
 loginBtn.onclick = () => {
-    dropLogin.classList.toggle("drop-login-open")
-}
+    dropLogin.classList.toggle("drop-login-open");
+};
 
 
 //menu open close
@@ -17,14 +17,14 @@ let menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
     menu.classList.toggle("move");
 
-}
+};
 
 //Home Swiper
 var swiper = new Swiper("#home", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 10000,
+      delay: 4000,
       disableOnInteraction: false,
     },
     pagination: {
@@ -33,3 +33,55 @@ var swiper = new Swiper("#home", {
     },
     
   });
+
+//Accordion
+
+const accordionItems = document.querySelectorAll(".accordion-item") ;
+
+accordionItems.forEach((item)=>{
+  const accordionHeader = item.querySelector(".accordion-header");
+  
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector(".accordion-open");
+    
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  }
+  else{
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
+
+
+//Control panel swiper
+var swiper = new Swiper(".control-image ", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
